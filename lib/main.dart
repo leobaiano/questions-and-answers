083993632454abae8a2e232c8bb061c8f30b1f67
Leo Baiano <ljunior2005@gmail.com>
2023-05-24 19:54:45 -0300

    feat: add questions and answers

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const AnswersApp());
-}
+void main() => runApp(AnswersApp());
 
 class AnswersApp extends StatelessWidget {
-  const AnswersApp({super.key});
+  final List<String> questions = [
+    'What is your favorite color?',
+    'What is your favorite animal?'
+  ];
+
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('Questions and Answers!'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Questions"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(questions[0]),
+            const ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            const ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+            const ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: null,
+            ),
+          ],
+        )
+      ),
     );
   }
-  
 }
